b4542be3bf1e6e4368a10cb3cd92692253dc144d
Indraacan <9haanacan@gmail.com>
2020-05-17 23:46:41 +0700

    new

--- validation/Register.js
+++ validation/Register.js
@@ -30,7 +30,9 @@ module.exports = function validateRegisterInput(data) {
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
   }
-  
+  if (!Validator.isLength(data.password, { min: 8, max: 30 })) {
+    errors.password = "Password must be at least 8 characters";
+  }
 
 return {
     errors,
