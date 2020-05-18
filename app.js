96f6f73495ff90e2992ad2d11787efdce2c74e80
Indraacan <9haanacan@gmail.com>
2020-05-18 23:47:45 +0700

    new

--- app.js
+++ app.js
@@ -29,6 +29,11 @@ app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
 app.use(cookieParser());
 app.use("/public", express.static("public"));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/user', UserRouter);
