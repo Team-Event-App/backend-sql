cb3b2d5f27f4901656fc7aefe34d73a5a4fd8b2e
Indraacan <9haanacan@gmail.com>
2020-05-24 23:34:37 +0700

    add new

--- migrations/20200509072119-create-event.js
+++ migrations/20200509072119-create-event.js
@@ -18,6 +18,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       imageEvent: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       organizerName: {
--- migrations/20200510122608-create-payment.js
+++ migrations/20200510122608-create-payment.js
@@ -15,6 +15,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       imageProof: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       bookingId: {
